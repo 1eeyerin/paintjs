04836b1b174250639516cfbf1f492e04c3756a49
ye_r1 <ye_r1@daum.net>
2019-10-15 20:55:00 +0900

    2.2 Recap

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const canvas = document.getElementById('jsCanvas');
 const  ctx = canvas.getContext('2d');
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
 
 
-function stopPainting() {
-  painting = false;
-}
-
-function startPainting() {
-  painting = true;
-}
+function stopPainting(event) {painting = false}
+function startPainting(event) {painting = true}
+function onMouseDown(event) {painting = true}
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   console.log(x,y);
-  if(!painting) {
-    ctx.beginPath();
+  if(!painting) { //페인팅 중이 아닐때
+    ctx.beginPath();  //line = path
     ctx.moveTo(x,y);  //인자로 받는 값
-  } else {
+  } else {  //페인팅 중 일때
     ctx.lineTo(x,y); //현재 sub-path의 마지막 점을 특정 좌표와 !직선!으로 연결한다.
-    ctx.stroke();
+    ctx.stroke(); //현재 sub-path를 현재의 stroke style로 획을 그음
   }
-}
-
-function onMouseDown(event) {
-  painting = true;
 }
 
 if(canvas) {
